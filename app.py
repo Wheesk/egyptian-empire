2417b7bf71bd3355795b8f9dfd1cf19502131738
Wheesk <sardorsardoreodb@gmail.com>
2025-07-09 17:42:45 +0200

    css update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown(
     """
     <style>
     [data-testid="stAppViewContainer"] {
-        background-image: url("https://images.unsplash.com/photo-1506744038136-46273834b3fb?fit=crop&w=1500&q=80");
+        background-image: url("https://images.unsplash.com/photo-1705628080778-f86b2f90a114?q=80&w=2338&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
